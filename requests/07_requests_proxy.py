7b8e318de016ad3b2d4deacaa763e433b7ad5c9c
RecluseXU <444640050@qq.com>
2020-02-21 00:03:53 +0800

    2020年2月21日00:03:41 开始学习scrapy

--- requests/07_requests_proxy.py
+++ requests/07_requests_proxy.py
@@ -2,8 +2,8 @@ import requests
 
 url = "https://cn.bing.com/"
 headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'
 }
-free_proxy = {'http':'27.17.45.90:43411'}
+free_proxy = {'http': '27.17.45.90:43411'}
 
-response = requests.get(url= url, headers=headers, proxies = free_proxy)
+response = requests.get(url=url, headers=headers, proxies=free_proxy)

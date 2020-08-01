55e73691e142551fc7373c59ad78b113a6300713
RecluseXU <444640050@qq.com>
2020-08-01 18:33:33 +0800

    修复$被修改而导致的各种问题

---
+++ web_example/DATA_WEB/MHW_DATA_FLASK/static/js/page_change.js
@@ -0,0 +1,60 @@
+/* 
+用来页面内容事件
+*/
+
+var $$ = jQuery
+function api_ajax(operation, params, before_func, suc_callback_func, err_callback_func){
+    $$.ajax({
+        url: "/api/" + operation,
+        dataType: "text",
+        async: true, //请求是否异步，默认为异步，这也是ajax重要特性
+        data: params,
+        type: "GET",
+        timeout: 1200,
+        cache: true,
+        beforeSend: before_func,
+        success: suc_callback_func,
+        error: err_callback_func,
+    });
+}
+
+function change_main_menu(){
+    var _menu = $$("#main-menu");
+    api_ajax(
+        "MainMenu",
+        {"block": jQuery("#block").attr("name")},
+        function(){
+            _menu.empty();
+        },
+        function(s){
+            var data = $$.parseJSON(s).data.items;
+            for (var i in data){
+                var a = "<li><a class=\"waves-effect waves-dark\" id=\"" + data[i].ID + "\"><i class=\"" + data[i].IconClass + "\"></i>\n" + data[i].Text + "</a></li>";
+                _menu.append(a);
+                $$("#" + data[i].ID).click(new Function(data[i].OnClick));
+            }
+        },
+        function(){}
+    );
+}
+
+function change_inner(id) {
+    var _inner = $$("#page-inner");
+    if(_inner.name == id){return null;}
+    api_ajax(
+        "Inner",
+        {"block":  $$("#block").attr("name"), "keyword": id},
+        function(){
+            _inner.empty();
+            _inner.name = "";
+            for(var i in document.getElementsByClassName('active-menu waves-effect waves-dark')){
+                i.class = "waves-effect waves-dark";}
+        },
+        function(data){
+            _inner.append(data);
+            _inner.name = id;
+            $$("#" + id).attr("class","active-menu waves-effect waves-dark");
+        },
+        function(){}
+    );
+}

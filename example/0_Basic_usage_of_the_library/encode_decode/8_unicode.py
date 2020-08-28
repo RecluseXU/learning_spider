25059aad83ebed120458469bbf3681c1ad80b777
RecluseXU <444640050@qq.com>
2020-08-29 02:06:12 +0800

    unicode编码操作 与 html转义操作

---
+++ example/0_Basic_usage_of_the_library/encode_decode/8_unicode.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   8_unicode.py
+@Time    :   2020-8-29 00:21:05
+@Author  :   Recluse Xu
+@Version :   1.0
+@Contact :   444640050@qq.com
+@Desc    :   基础unicode操作
+'''
+
+
+def char_2_unicode_char(char: str) -> str:
+    # 字符编码为unicode字符
+    c = char.encode('unicode_escape')
+    c = str(c)
+    print(char, 'Unicode编码结果', c)
+    return c
+
+
+def unicode_char_2_char(unicode_char: str) -> str:
+    # unicode字符解码为字符
+    c = unicode_char.encode().decode('unicode_escape')
+    print(unicode_char, 'Unicode编码结果', c)
+    return c
+
+
+def char_2_unicode_ret(a_char: str) -> int:
+    # 得到一个字符的unicode 数值
+    c = ord(a_char)
+    print(a_char, '对应Unicode值', c)
+    return c
+
+
+def unicode_ret_2_char(a_ret: int) -> int:
+    # 根据unicode 数值，得到一个字符
+    c = chr(a_ret)
+    print(a_ret, '对应字符', c)
+    return c
+
+
+if __name__ == "__main__":
+    c = '齤'
+    char_2_unicode_char(c)
+    unicode_char_2_char('\\u4f60\\u597d')
+
+    char_2_unicode_ret(c[0])
+    unicode_ret_2_char(20320)
---
+++ example/0_Basic_usage_of_the_library/encode_decode/9_html.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   9_html.py
+@Time    :   2020/08/27 12:05:58
+@Author  :   Recluse Xu
+@Version :   1.0
+@Contact :   444640050@qq.com
+@Desc    :   html转义相关操作
+'''
+
+# here put the import lib
+import html
+
+
+def htmlparser_2_str(html_str: str):
+    _str = html.unescape(html_str)
+    print(_str)
+    return _str
+
+
+def str_2_htmlparser(_str: str):
+    html_str = html.escape(_str)
+    print(html_str)
+    return html_str
+
+
+if __name__ == "__main__":
+    html_str = '&nbsp;&lt;&gt;'
+    a = htmlparser_2_str(html_str)
+    str_2_htmlparser(a)

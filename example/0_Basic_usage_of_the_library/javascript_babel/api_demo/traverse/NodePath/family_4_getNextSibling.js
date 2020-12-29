64d68aba4d5b7b87cd58406541b281298b17e311
RecluseXU <444640050@qq.com>
2020-12-29 09:15:50 +0800

    Babel样例更新

---
+++ example/0_Basic_usage_of_the_library/javascript_babel/api_demo/traverse/NodePath/family_4_getNextSibling.js
@@ -0,0 +1,25 @@
+// NodePath.getNextSibling()获取同级后一个节点的NodePath
+// @return Node
+// 此函数源码就一句 return this.getSibling(this.key + 1);
+
+var js_env = "E:/Software/Programming/Environment/Nodejs/node_global/node_modules/";
+const parser = require(js_env + "@babel/parser");
+const traverse = require(js_env + "@babel/traverse").default;
+
+const jscode = `
+  var a = 1 + 9;
+  a = a + a;
+  console.log(a);
+  console.log(b);
+`;
+
+const ast = parser.parse(jscode);
+const visitor = {
+  ExpressionStatement(path) {
+    console.log('当前节点源码:\n', path.toString())
+    console.log('同级前一个节点源码:\n', path.getNextSibling().toString())
+    console.log('----------------')
+  }
+}
+
+traverse(ast, visitor);
